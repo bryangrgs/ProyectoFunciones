7a50037b1f636897a40df1a89345b91c7c065c11
bryangrgs <103081416+bryangrgs@users.noreply.github.com>
2024-05-04 16:59:02 -0300

    Ingresando cambios a principal

--- src/principal.cpp
+++ src/principal.cpp
@@ -16,6 +16,46 @@ using namespace std;
 
 int main()
 {
+<<<<<<< HEAD
+   /*
+    cout<< length("Pirulito")<<endl;
+    tutorial  1+i
+    cout<< charCount("Apap.ku 6apa", 'x');
+    */
+    string s="patata en casa en arg";
+    /*
+    cout<< substring(s,2,length(s))<<endl;
+    cout<< substring(s,5)<<endl;
+
+    int p = indexOf(s,'c');
+    cout << p << endl;
+
+    int p = indexOf(s,'a',5);
+    cout << p << endl;
+
+    int p = indexOf(s,"en");
+    cout << p << endl;
+
+    int p = indexOf(s,"ta",3);
+    cout << p << endl;
+
+    int p = lastIndexOf(s,'e');
+    cout << p << endl;
+      char palabrita='D';
+      int valor= charToInt(palabrita);
+      cout << valor << endl;
+      int numerito=65;
+         char letrita= intToChar(numerito);
+         cout << letrita << endl;
+
+      int numerote=4587412;
+      int posicion=0;
+      int digito= getDigit(numerote,posicion);
+      cout<< digito<<endl;
+      int digitocontador= digitCount(numerote);
+      cout<< digitocontador<<endl;
+      */
+=======
    cout<< length("Pirulito")<<endl;
    //tutorial  1+i
    cout<< charCount("Apap.ku 6apa", 'x')<<endl;
@@ -30,6 +70,7 @@ int main()
    int numerito=65;
       char letrita= intToChar(numerito);
       cout << letrita << endl;
+>>>>>>> 1d3f19ed034dc238c1d40c8c5c5bb1baf4bc1453
    return 0;
 }
 
