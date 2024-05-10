552a0b26229b54182c35609cbb6fd1a8577cf817
Sophii15 <168750570+Sophii15@users.noreply.github.com>
2024-05-09 23:47:11 -0300

    Update varios principal.cpp

--- src/principal.cpp
+++ src/principal.cpp
@@ -16,69 +16,158 @@ using namespace std;
 
 int main()
 {
-
    /*
-    cout<< length("Pirulito")<<endl;
-    tutorial  1+i
-    cout<< charCount("Apap.ku 6apa", 'x');
-    */
-    string s="patata en casa en arg";
-    /*
-    cout<< substring(s,2,length(s))<<endl;
-    cout<< substring(s,5)<<endl;
+      cout<< length("Pirulito")<<endl;
+      tutorial  1+i
+      cout<< charCount("Apap.ku 6apa", 'x');
 
-    int p = indexOf(s,'c');
-    cout << p << endl;
+      string s="patata en casa en arg";
 
-    int p = indexOf(s,'a',5);
-    cout << p << endl;
+      cout<< substring(s,2,length(s))<<endl;
+      //cout<< substring(s,5)<<endl;
 
-    int p = indexOf(s,"en");
-    cout << p << endl;
+      int p = indexOf(s,'c');
+      cout << p << endl;
 
-    int p = indexOf(s,"ta",3);
-    cout << p << endl;
+      int p = indexOf(s,'a',5);
+      cout << p << endl;
 
-    int p = lastIndexOf(s,'e');
-    cout << p << endl;
+      int p = indexOf(s,"en");
+      cout << p << endl;
+
+      int p = indexOf(s,"ta",3);
+      cout << p << endl;
+
+      int p = lastIndexOf(s,'e');
+      cout << p << endl;
+      
       char palabrita='D';
-      int valor= charToInt(palabrita);
-      cout << valor << endl;
+        int valor= charToInt(palabrita);
+        cout << valor << endl;
+        
       int numerito=65;
-         char letrita= intToChar(numerito);
-         cout << letrita << endl;
+      char letrita= intToChar(numerito);
+      cout << letrita << endl;
 
       int numerote=4587412;
       int posicion=0;
       int digito= getDigit(numerote,posicion);
       cout<< digito<<endl;
+        
       int digitocontador= digitCount(numerote);
       cout<< digitocontador<<endl;
 
-    int numero=123;
-    string resultado = intToString(numero);
-    cout<<resultado<<endl;
+      int numero=123;
+      string resultado = intToString(numero);
+      cout<<resultado<<endl;
 
-    int b=16; //base del numero si es binario-hexadecimal
-    string cadenaStringtoInt ="12AB"; //Ingresar binarios decimal o hexa
-    int iStringtoint= stringToInt(cadenaStringtoInt,b); 
-    cout<<iStringtoint<<endl;
+      int b=16;
+      string cadenaStringtoInt ="12AB"; //Ingresar binarios decimal o hexa
+      int iStringtoint= stringToInt(cadenaStringtoInt,b);
+      cout<<iStringtoint<<endl;
 
-    string ssobrecar="12323";
-    int istringtoubtsobre= stringToInt(ssobrecar);
-    cout<<istringtoubtsobre<<endl;
+      string ssobrecar="12323";
+      int istringtoubtsobre= stringToInt(ssobrecar);
+      cout<<istringtoubtsobre<<endl;
 
-    char cchartistring = ' ';
-    string sChartostring = charToString(cchartistring);
-    cout << sChartostring << endl; // muestra: A
-    cout << length(sChartostring) << endl; // muestra: 1
-   return 0;
+      char cchartistring = ' ';
+      string sChartostring = charToString(cchartistring);
+      cout << sChartostring << endl; // muestra: A
+      cout << length(sChartostring) << endl; // muestra: 1
+        return 0;
 
-    string sStringtochar = " ";
-    char cstringtochar = stringToChar(sStringtochar);
-    cout << cstringtochar << endl; // muestra: A
-    cout << (int)cstringtochar << endl; // muestra: 65
-    */
+      string sStringtochar = " ";
+      char cstringtochar = stringToChar(sStringtochar);
+      cout << cstringtochar << endl; // muestra: A
+      cout << (int)cstringtochar << endl; // muestra: 65
+
+      string s = "10";
+      int i = stringToInt(s,10);
+      cout << i << endl;
+      int i = stringToInt(s);
+      cout << i << endl;
+
+      string s = stringToString("Hola");
+      cout << s << endl;
+
+      double d = 123.4;
+      string s = doubleToString(d);
+      cout << s << endl;
+
+      string s = "123.4";
+      double d = stringToDouble(s);
+      cout << d << endl;
+
+      string s = "";
+      cout << isEmpty(s) << endl;
+
+      string s1 = "cursoDeAlgoritmos";
+      string s2 = "c";
+      if( startsWith(s1,s2) )
+      {
+         cout << s2 << " es prefijo de: " << s1 << endl;
+      }
+      else {
+         cout<<s2<<" no es prefijo "<<s1<<endl;
+      }
+
+      string s1 = "curso de patatas";
+      string s2 = "patatas";
+      if( endsWith(s1,s2) )
+      {
+      cout << s2 << " es sufijo de: " << s1 << endl;
+      }
+      else {
+         cout<<s2<<" no es sufijo "<<s1<<endl;
+      }
+
+      string s = "abcd";
+      char c = 'a';
+      if( contains(s,c) )
+      {
+         cout << s << " contiene a: " << c << endl;
+      }
+      else
+      {
+         cout << s << " NO contiene a: " << c << endl;
+      }
+
+      string s = "Esto es una prueba";
+      string r = replace(s,'e','X');
+      cout << r << endl;
+
+      string s = "Esto es una prueba";
+      int pos = 4;
+      char c = 'X';
+      string r = insertAt(s,pos,c);
+      cout << r << endl;
+
+      int n = 5;
+      string r = spaces(n);
+      // muestra: [ ] (cinco espacios)
+      cout << "[" << r << "]" << endl;
+
+   char c = '3';
+   if( isDigit(c) )
+   {
+   cout << c << " es digito" << endl;
+   }
+   else
+   {
+   cout << c << " NO es digito" << endl;
+   }
+
+   char c = 'X';
+   if( isLetter(c) )
+   {
+   cout << c << " es letra" << endl; // SALIDA
+   }
+   else
+   {
+      cout << c << " NO es letra" << endl;
+   }
+  }
+*/
 }
 
 #endif
