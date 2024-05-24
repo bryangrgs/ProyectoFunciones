0dfa5488264ab1fd88f8f3828c64c57e35210f32
bryangrgs <103081416+bryangrgs@users.noreply.github.com>
2024-05-24 10:12:46 -0300

    Update tokens.hpp

--- src/biblioteca/funciones/tokens.hpp
+++ src/biblioteca/funciones/tokens.hpp
@@ -15,11 +15,42 @@ void addToken(string& s,char sep,string t)
 {
 }
 
+
 string getTokenAt(string s,char sep, int i)
 {
-   return "";
-}
+    int cantidadToken = tokenCount(s,sep);
+    string token;
 
+    // caso si se quiere el token 0
+    if(i == 0)
+    {
+        int posPrimerSep = indexOf(s,sep);
+        token = substring(s,0,posPrimerSep);
+    }
+    else
+    {
+        // caso si se quiere al ultimo token
+        if(i == cantidadToken - 1)
+        {
+            // le sumo uno a posUltimoSep porque la funcion substring incluye este caracter, y no queremos que incluya sep
+            int posUltimoSep = lastIndexOf(s,sep) + 1;
+            int posFin = indexOf(s,'\0');
+            token = substring(s,posUltimoSep,posFin);
+        }
+
+        // caso si se quiere cualquier token del medio
+        else
+        {
+            // le tengo que sumar 1 a posIniSep porque la funcion substring incluye este caracter, y no queremos que incluya el caracter sep
+            int posIniSep = indexOfN(s,sep,i) + 1;
+            // le sumo uno a i porque quiero entre la pos i e i + 1
+            int posFinSep = indexOfN(s,sep,i + 1);
+
+            token = substring(s,posIniSep,posFinSep);
+        }
+    }
+    return token;
+}
 void removeTokenAt(string& s,char sep, int i)
 {
 }
