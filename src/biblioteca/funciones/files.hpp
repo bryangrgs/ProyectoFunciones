e1c6ccf6814b87c68b0292f2bce195cc4dacf462
bryangrgs <103081416+bryangrgs@users.noreply.github.com>
2024-07-24 03:01:37 -0300

    Update files.hpp "funcionando"

--- src/biblioteca/funciones/files.hpp
+++ src/biblioteca/funciones/files.hpp
@@ -6,36 +6,39 @@
 
 template<typename T> void write(FILE* f, T t)
 {
-   // Se debe anteponer esta linea al inicio de la funcion.
-   // De otro modo, apareceran errores al momento de actualizar
-   // archivos. Esto se debe a un error en la implementacion de Windows.
-   //
-   // fseek(f,0,SEEK_CUR);
+    fseek(f,0,SEEK_CUR);
+    fwrite(&t,sizeof(t),1,f);
+
 }
 
 template<typename T> T read(FILE* f)
 {
-   // Se debe anteponer esta linea al inicio de la funcion.
-   // De otro modo, apareceran errores al momento de actualizar
-   // archivos. Esto se debe a un error en la implementacion de Windows.
-   //
-   // fseek(f,0,SEEK_CUR);
+
+    fseek(f,0,SEEK_CUR);
 
    T t;
+   fread(&t,sizeof(t),1,f);
    return t;
 }
 
 template<typename T> void seek(FILE* f, int n)
 {
+   T t = read<T> (f);
+   fseek(f,sizeof(t)*n,SEEK_SET);
 }
 
 template<typename T> int fileSize(FILE* f)
 {
-   return 0;
+   fseek(f,0,SEEK_END);
+   int size= ftell(f)/sizeof(T);
+   return size;
 }
 
 template<typename T> int filePos(FILE* f)
 {
+   int pos= ftell(f)/sizeof(T);
+  return pos;
+
    return 0;
 }
 
