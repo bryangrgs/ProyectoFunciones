b2cfb7c3bcbba01cb0246d3d3721ff9ee510de01
mloiaconoUtn <168829957+mloiaconoUtn@users.noreply.github.com>
2024-09-19 06:16:08 -0300

    Update Map.hpp

--- src/biblioteca/tads/parte2/Map.hpp
+++ src/biblioteca/tads/parte2/Map.hpp
@@ -1,4 +1,3 @@
-
 #ifndef _TMAP_TAD_
 #define _TMAP_TAD_
 
@@ -7,95 +6,174 @@
 
 using namespace std;
 
-template<typename K,typename V>
+template<typename K,typename T>
 struct Map
 {
    Array<K> keys;
-   Array<V> values;
+   Array<T> values;
+   int curr;
 };
 
 template<typename K,typename V>
 Map<K,V> map()
 {
    Map<K,V> m;
+   m.keys = array<K>();
+   m.values=array<V>();
+   m.curr=0;
    return m;
 }
 
+template<typename K>
+int cmpKK(K a,K b)
+{
+   return a<b?-1:a==b?0:1;
+}
+template<typename V>
+int cmpVV(V a,V b)
+{
+   return a<b;
+}
 template<typename K,typename V>
 V* mapGet(Map<K,V> m,K k)
 {
-   return NULL;
+   int p = arrayFind<K,K>(m.keys,k,cmpKK);
+   return p>=0?arrayGet<V>(m.values,p):NULL;
 }
 
 template<typename K,typename V>
 V* mapPut(Map<K,V>& m,K k,V v)
 {
-   return NULL;
+   int p = arrayFind<K,K>(m.keys,k,cmpKK);
+   if( p<0 )
+   {
+      arrayAdd<K>(m.keys,k);
+      arrayAdd<V>(m.values,v);
+   }
+   else
+   {
+      arraySet<V>(m.values,p,v);
+   }
+
+   return mapGet<K,V>(m,k);
 }
 
 template<typename K,typename V>
 bool mapContains(Map<K,V> m,K k)
 {
-   return true;
+   return mapGet<K,V>(m,k)!=NULL;
 }
 
 template<typename K,typename V>
 V mapRemove(Map<K,V>& m,K k)
 {
-   V v;
-   return v;
+   int p = arrayFind<V,K>(k.keks,k,cmpKK);
+   arrayRemove<K>(m.keys,p);
+   V t = arrayRemove<V>(m.values,p);
+   return t;
 }
 
 template<typename K,typename V>
 void mapRemoveAll(Map<K,V>& m)
 {
+   arrayRemoveAll<K>(m.keys);
+   arrayRemoveAll<V>(m.values);
 }
 
 template<typename K,typename V>
 int mapSize(Map<K,V> m)
 {
-   return 0;
+   return arraySize<K>(m.keys);
 }
 
 template<typename K,typename V>
 bool mapHasNext(Map<K,V> m)
 {
-   return true;
+   int x = arraySize<K>(m.keys);
+   return m.curr<x;
 }
 
 template<typename K,typename V>
 K mapNextKey(Map<K,V>& m)
 {
-   K k;
-   return k;
+   return *arrayGet<K>(m.keys,m.curr++);
 }
 
 template<typename K,typename V>
 V* mapNextValue(Map<K,V>& m)
 {
-   return NULL;
+   return arrayGet<V>(m.values,m.curr++);
 }
 
 template<typename K,typename V>
 void mapReset(Map<K,V>& m)
 {
+   m.curr=0;
 }
 
 template<typename K,typename V>
 V* mapDiscover(Map<K,V>& m,K k,V v)
 {
-   return NULL;
-}
+   V* x = mapGet<K,V>(m,k);
+   if( x==NULL )
+   {
+      x = mapPut<K,V>(m,k,v);
+   }
 
+   return x;
+}
 
 template<typename K,typename V>
 void mapSortByKeys(Map<K,V>& m,int cmpKK(K,K))
 {
+   bool ordenado=true;
+   while(!ordenado)
+   {
+   ordenado=false;
+   for(int i=0; i<mapSize(m); i++)
+   {
+      if( cmpKK(m.keys.arr[i],m.keys.arr[i+1])>0 )
+      {
+         K k = m.keys.arr[i];
+         m.keys.arr[i] = m.keys.arr[i+1];
+         m.keys.arr[i+1] = k;
+
+         V v= m.values.arr[i];
+         m.values.arr[i] = m.values.arr[i+1];
+         m.values.arr[i+1] = v;
+
+         ordenado=true;
+      }
+   }
+}
+
 }
 
 template<typename K,typename V>
 void mapSortByValues(Map<K,V>& m,int cmpVV(V,V))
 {
+   bool ordenado=true;
+      while(!ordenado)
+      {
+      ordenado=false;
+      for(int i=0; i<mapSize(m); i++)
+      {
+         if( cmpVV(m.values.arr[i],m.values.arr[i+1])>0 )
+         {
+            K k = m.keys.arr[i];
+            m.keys.arr[i] = m.keys.arr[i+1];
+            m.keys.arr[i+1] = k;
+
+            V v = m.values.arr[i];
+            m.values.arr[i] = m.values.arr[i+1];
+            m.values.arr[i+1] = v;
+
+            ordenado=true;
+         }
+      }
+   }
+
+
 }
 
 #endif
